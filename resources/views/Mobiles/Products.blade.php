69675c1764675165959bb82487f84b9400c544d9
AnshuAlawa <alawapappu@gmail.com>
2023-07-14 10:39:27 +0530

    cart desing in product

--- resources/views/Mobiles/Products.blade.php
+++ resources/views/Mobiles/Products.blade.php
@@ -16,19 +16,42 @@
         @foreach ($product as $item)
             <div class="card-group">
                 <div class="card">
-                    <img class="" src="holder.js/100x180/" alt="{{ $item->product_name }}">
+                    <img class="" src="{{ asset('img/mobile.png') }}" alt="{{ $item->product_name }}">
                     <div class="card-body">
-                        <h3 class="card-title">{{ $item->product_name }}</h3>
-                        <h4 class="card-title">{{ $item->price }}</h4>
-                        <h4 class="card-title">{{ $item->special_offers }}</h4>
-                        <p class="card-title">{{ $item->brand }}</p>
-                        <p class="card-title">{{ $item->category }}</p>
-                        <p class="card-title">{{ $item->availability }}</p>
-                        <p class="card-title">{{ $item->ratings }}</p>
-                        <p class="card-title">{{ $item->warranty }}</p>
-                        <p class="card-title">{{ $item->accessories }}</p>
+                        <div class="row">
+                            <h3 class="card-title">{{ $item->product_name }}</h3>
+                            <div class="col-lg-6"></div>
+                            <div class="col-lg-6 text-end">
+                                <a name="" id="" class="btn btn-warning mx-5" href=""
+                                    role="button">
+                                    ADD TO CART</a>
+                                <a name="" id="" class="btn btn-warning mx-5" href=""
+                                    role="button">
+                                    BUY NOW</a>
+                            </div>
+                        </div>
+                        <h4 class="card-title"><span class="text-info"> Price :- </span> Rs . <s> {{ $item->price }}
+                                /-</s></h4>
+                        <h4 class="card-title"><span class="text-info"> Specail Offers :-
+                            </span>Rs. {{ $item->special_offers }} /-</h4>
+                        <p class="card-title"><span class="text-info"> Brand :- </span> {{ $item->brand }}</p>
+                        <p class="card-title"><span class="text-info"> Category :- </span>{{ $item->category }}</p>
+                        <p class="card-title"><span class="text-info"> Availability :- </span>
+                            <?php
+                            if ($item->availability == 'In Stock') {
+                                echo '<span class="bg-success shadow p-1 text-light rounded">In Stock</span>';
+                            } else {
+                                echo '<span class="bg-warning shadow p-1 text-light rounded">Out of Stock</span>';
+                            }
+                            ?>
+                        </p>
+                        <p class="card-title"><span class="text-info"> Rating :- </span> {{ $item->ratings }}</p>
+                        <p class="card-title"><span class="text-info"> Warranty :- </span> {{ $item->warranty }}</p>
+                        <p class="card-title"><span class="text-info"> Accessories :- </span> {{ $item->accessories }}
+                        </p>
 
-                        <p class="card-text">{{ $item->description }}</p>
+                        <p class="card-text"><span class="text-info"> Description :- </span>{{ $item->description }}
+                        </p>
                     </div>
                 </div>
             </div>
--- resources/views/customComponent/nav-bar.blade.php
+++ resources/views/customComponent/nav-bar.blade.php
@@ -1,10 +1,16 @@
-<nav class="nav justify-content-center flex-row text-center bg-primary shadow p-1">
+<nav class="nav justify-content-center flex-row text-center bg-primary shadow p-1 fixed-top">
     <div class="col-4"></div>
     <div class="col-4 ">
-        <span class="mx-4 "><a href="{{ url('/') }}" class="btn btn-primary">Home</span>
+        <span class="mx-4 "><a href="{{ url('/') }}" class="btn btn-primary">Home</a></span>
         <span class="mx-4 "><a href="" class="btn btn-primary">Mobile</a></span>
-        <span class="mx-4 "><a href="" class="btn btn-primary">Profile</a></span>
-        <span class="mx-4 "><a href="" class="btn btn-primary">About</a></span>
+        <?php
+        if (session('role') == 'admin') {
+            echo '<span class="mx-4 "><a href="/adminboard" class="btn btn-primary">Dashboard</a></span>';
+        } else {
+            echo '<span class="mx-4 "><a href="" class="btn btn-primary">Cart</a></span>';
+            echo '<span class="mx-4 "><a href="" class="btn btn-primary">About</a></span>';
+        }
+        ?>
     </div>
     <div class="col-4 text-end px-5">
         <span>{{ session('role') }} : </span>
